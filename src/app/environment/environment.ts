1d26209ee1ecae94e7e819d0f5e7f7e98e920897
José Antonio Domínguez gonzález <joseantdominguezgonzalez@gmail.com>
2026-02-24 18:23:56 +0100

    security

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,17 +1,15 @@
-
-
 export const environment = {
-    production: true,
-    CLOUD_NAME: "dnre5bukm",
-    UPLOAD_PRESET: "MappTuu",
-    CLOUDINARY_API_KEY: "653478338367121",
-    firebase: {
-    apiKey: "AIzaSyC0iCkAwrhlSZ5C5-g01ZbTbupV_3Kh9ew",
-    authDomain: "maptuu-e0f68.firebaseapp.com",
-    projectId: "maptuu-e0f68",
-    storageBucket: "maptuu-e0f68.firebasestorage.app",
-    messagingSenderId: "894446902771",
-    appId: "1:894446902771:web:59914a0a4c8ab97b218686",
-    measurementId: "G-PJC4K4MV6P"
-    }
-  };
+  production: true,
+  CLOUD_NAME: (window as any)['NG_APP_CLOUDINARY_CLOUD_NAME'] || '',
+  UPLOAD_PRESET: 'MappTuu',
+  CLOUDINARY_API_KEY: '653478338367121',
+  firebase: {
+    apiKey: 'AIzaSyC0iCkAwrhlSZ5C5-g01ZbTbupV_3Kh9ew',
+    authDomain: 'maptuu-e0f68.firebaseapp.com',
+    projectId: 'maptuu-e0f68',
+    storageBucket: 'maptuu-e0f68.firebasestorage.app',
+    messagingSenderId: '894446902771',
+    appId: '1:894446902771:web:59914a0a4c8ab97b218686',
+    measurementId: 'G-PJC4K4MV6P',
+  },
+};
