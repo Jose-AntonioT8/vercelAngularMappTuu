3fad1b31ce532ce76b56cdb20fad326f6c84202a
José Antonio Domínguez gonzález <joseantdominguezgonzalez@gmail.com>
2026-02-24 18:36:51 +0100

    seguridad

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   CLOUD_NAME: (window as any)['NG_APP_CLOUDINARY_CLOUD_NAME'] || '',
-  UPLOAD_PRESET: 'MappTuu',
+  UPLOAD_PRESET: (window as any)['NG_APP_UPLOAD_PRESET'] || '',
   CLOUDINARY_API_KEY: '653478338367121',
   firebase: {
-    apiKey: 'AIzaSyC0iCkAwrhlSZ5C5-g01ZbTbupV_3Kh9ew',
-    authDomain: 'maptuu-e0f68.firebaseapp.com',
-    projectId: 'maptuu-e0f68',
-    storageBucket: 'maptuu-e0f68.firebasestorage.app',
-    messagingSenderId: '894446902771',
-    appId: '1:894446902771:web:59914a0a4c8ab97b218686',
-    measurementId: 'G-PJC4K4MV6P',
+    apiKey: (window as any)['NG_APP_API_KEY'] || '',
+    authDomain: (window as any)['NG_APP_AUTH_DOMAIN'] || '',
+    projectId: (window as any)['NG_APP_PROJECT_ID'] || '',
+    storageBucket: (window as any)['NG_APP_STORAGE_BUCKET'] || '',
+    messagingSenderId: (window as any)['NG_APP_MESSAGING_SENDER_ID'] || '',
+    appId: (window as any)['NG_APP_APP_ID'] || '',
+    measurementId: (window as any)['NG_APP_MEASUREMENT_ID'] || '',
   },
 };
